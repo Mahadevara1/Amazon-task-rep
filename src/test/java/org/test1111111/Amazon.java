7fd00e2bc7c2a4dae84e3c7cae643cb9c8739837
Mahadevara1 <mahadevara1@gmail.com>
2019-09-20 05:18:45 +0530

    Amazontask

---
+++ src/test/java/org/test1111111/Amazon.java
@@ -0,0 +1,33 @@
+package org.test1111111;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Amazon extends AmazonBase {
+	public static void main(String[] args) throws Throwable {
+		launchBrowser();
+		
+	load("https://www.amazon.com");
+	 screen("E:\\screenshot\\.amazon1.png");
+	 Thread.sleep(2000);
+	 WebElement search = driver.findElement(By.id("twotabsearchtextbox"));
+		fill(search,getData(0,0));
+		screen("E:\\screenshot\\.amazon2.png");
+		 Thread.sleep(2000);
+	WebElement search1 = driver.findElement(By.className("nav-input"));
+	
+		btnclick(search1);
+		List<WebElement> text = driver.findElements(By.xpath("//span[@class='a-size-medium a-color-base a-text-normal']"));
+		for (WebElement x : text) {
+			System.out.println(x.getText());
+			screen("E:\\screenshot\\.amazon2.png");
+			
+		}
+		
+}
+
+
+}
